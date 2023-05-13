a872b0b41a51fd6020a29f6f5586baad1c73b6f7
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-05-13 22:58:57 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,26 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   // Your code goes here
-})
+   name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 1024,
+  },
+});
 
 const User = mongoose.model('user',userSchema);
 
